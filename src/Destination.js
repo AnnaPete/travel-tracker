72b0e9d7eb18a791aae820c0b449d47841f128cb
Anna Peterson <reveragb@gmail.com>
2023-01-10 18:08:05 -0700

    Modify flight cost per person

--- src/Destination.js
+++ src/Destination.js
@@ -2,8 +2,8 @@ class Destination {
   constructor(destination) {
     this.id = destination.id
     this.destination = destination.destination
-    this.lodgingCostPerDay = destination.lodgingCostPerDay
-    this.flightCostPerPerson = destination.flightCostPerPerson
+    this.lodgingCostPerDay = destination.estimatedLodgingCostPerDay
+    this.flightCostPerPerson = destination.estimatedFlightCostPerPerson
     this.image = destination.image
     this.alt = destination.alt
   }
