3aa31bb197dc6ea6b85e9c25bc26f570e3cd5523
Anna Peterson <reveragb@gmail.com>
2023-01-11 13:16:32 -0700

    Write out it blocks

--- test/agent-test.js
+++ test/agent-test.js
@@ -4,23 +4,27 @@ const expect = chai.expect;
 describe('Agent', function() {
   this.beforeEach();
 
-  it.skip('', function() {
+  it.skip('should see a list of travelers', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should see a list of trips', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should see a list of destinations', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should have a method that calculates the total income for the year', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should have a method that finds trips by year', function() {
+    expect().to.equal();
+  });
+
+  it.skip('should have a method that finds travelers by name', function() {
     expect().to.equal();
   });
 });
