d0cafddc162cb25c1af6441c5e9c13aa972d2bd2
Anna Peterson <reveragb@gmail.com>
2023-01-10 18:41:49 -0700

    Add beforeEach and test expectations

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -1,26 +1,41 @@
+import Traveler from '../src/Traveler';
+import Destination from '../src/Destination';
+import Trip from '../src/Trip';
+import Agent from '../src/Agent';
+import tripData from '../src/trip-data';
+import destinationData from '../src/destination-data';
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  this.beforeEach();
+describe('Traveler', function() {
+  this.beforeEach(() => {
+    const traveler1 = {id: 1, name: 'Gandalf', type: 'relaxer'}
+    let newTraveler = new Traveler(traveler1)
 
-  it.skip('', function() {
+    tripData.forEach(trip => {
+      const newTrip = new Trip(trip)
+      newTraveler.trips.push(newTrip)
+    })
+
+    // destinationData.forEach(destination => {
+    //   const newDestination = new Destination(destination)
+    //   agent.destinations.push(newDestination)
+    // })
+  });
+
+  it.skip('should be an instantiation of Traveler', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should sort the user trips array by date', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should calculate the cost for a single trip', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
-    expect().to.equal();
-  });
-
-  it.skip('', function() {
+  it.skip('should calculate the total spent on trips for a given year', function() {
     expect().to.equal();
   });
 });
--- test/trip-test.js
+++ test/trip-test.js
@@ -1,26 +1,64 @@
+import tripData from '../src/trip-data';
+import destinationData from '../src/destination-data';
+import Traveler from '../src/Traveler';
+import Trip from '../src/Trip'
+import Destination from '../src/Destination';
 import chai from 'chai';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  this.beforeEach();
+describe('Trip', function() {
+  this.beforeEach(() => {
+    const traveler1 = {id: 1, name: 'Gandalf', type: 'relaxer'}
+    let newTraveler = new Traveler(traveler1)
 
-  it.skip('', function() {
+    tripData.forEach(trip => {
+      const newTrip = new Trip(trip)
+      newTraveler.trips.push(newTrip)
+    })
+
+    // destinationData.forEach(destination => {
+    //   const newDestination = new Destination(destination)
+    //   agent.destinations.push(newDestination)
+    // })
+  });
+
+  it.skip('should have an id', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should have a user id', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should have a destination id', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should have a list of travelers', function() {
     expect().to.equal();
   });
 
-  it.skip('', function() {
+  it.skip('should have a date', function() {
+    expect().to.equal();
+  });
+
+  it.skip('should have a duration', function() {
+    expect().to.equal();
+  });
+
+  it.skip('should have a status', function() {
+    expect().to.equal();
+  });
+
+  it.skip('should have a list of suggested activities', function() {
+    expect().to.equal();
+  });
+
+  it.skip('should have a method to format the trip date', function() {
+    expect().to.equal();
+  });
+
+  it.skip('should have a method to determine the trip status', function() {
     expect().to.equal();
   });
 });
