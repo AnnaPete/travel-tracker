387e63201a1143693ebdc419571ff12406554292
Anna Peterson <reveragb@gmail.com>
2023-01-10 18:00:53 -0700

    Add destination class

---
+++ src/Destination.js
@@ -0,0 +1,12 @@
+class Destination {
+  constructor(destination) {
+    this.id = destination.id
+    this.destination = destination.destination
+    this.lodgingCostPerDay = destination.lodgingCostPerDay
+    this.flightCostPerPerson = destination.flightCostPerPerson
+    this.image = destination.image
+    this.alt = destination.alt
+  }
+}
+
+export default Destination;
