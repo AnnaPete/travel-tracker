421c71a5fb9a0801a8cb1c7b5ac841dd56a42fd1
Anna Peterson <reveragb@gmail.com>
2023-01-13 09:38:55 -0700

    Import class files and add sections

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,30 @@
 import './css/styles.css';
+// import fetchAPI from './api-calls';
+import Traveler from './Traveler';
+import Agent from './Agent';
+import Trip from './Trip';
+import Destination from './Destination';
+
+// QUERY SELECTORS
+
+
+// GLOBAL VARIABLES
+
+
+// EVENT LISTENERS
+
+
+// FETCH
+
+
+// INFORMATION DISPLAY
+
+
+// FORM FUNCTIONALITY
+
+
+// LOGIN/LOGOUT 
+
+
+// ERROR 
+
