b44cd66c9e89c49eb818b41a2bf923fa267e7a01
Anna Peterson <reveragb@gmail.com>
2023-01-13 11:42:35 -0700

    Build out api skeletons

---
+++ src/api-calls.js
@@ -0,0 +1,32 @@
+// GET
+const fetchAllTravelerData = () => {
+
+}
+
+const fetchSingleTravelerData = () => {
+
+}
+
+const fetchTripData = () => {
+
+}
+
+const fetchDestinationData = () => {
+
+}
+
+// POST
+const addNewTrip = () => {
+
+}
+
+const modifyTrip = () => {
+
+}
+
+// DELETE
+const deleteTrip = () => {
+  
+}
+
+export default {fetchAPIs};
